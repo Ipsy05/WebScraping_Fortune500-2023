cd0a94113843d7e3a3fd3a4c0dbe80e587fb93b8
Ipsita Pal <136154134+Ipsy05@users.noreply.github.com>
2024-02-03 16:13:05 +0530

    Add lib file

---
+++ lib.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Import necessary libraries
+from bs4 import BeautifulSoup  # BeautifulSoup is used for web scraping
+import requests  # Requests is used for making HTTP requests
+
+# Explanation:
+# - BeautifulSoup is a library for pulling data out of HTML and XML files.
+# - 'requests' is a popular Python library for making HTTP requests.
+# Import the pandas library and alias it as 'pd'
+import pandas as pd
+
+# Explanation:
+# - 'import pandas as pd' is a common convention to import the pandas library and alias it as 'pd' for easier reference.
+# - Pandas is widely used for data manipulation and analysis, and the alias 'pd' is a common shorthand in the Python community.
+
